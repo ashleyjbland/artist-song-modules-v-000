454724009e38522cbfb4db5235b7c337f3561f0f
Ashley Bland <ashleyjbland@gmail.com>
2018-09-28 21:17:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,9 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    self.class.all << self
     @songs = []
   end
-
-  #def self.find_by_name(name)
-  #  @@artists.detect{|a| a.name == name}
-  #end
 
   def self.all
     @@artists
@@ -31,7 +27,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
-  #end
 end
