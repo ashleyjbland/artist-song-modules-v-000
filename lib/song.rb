4f52ca0fe9ecab5c2949e4f08e1da9af965cb3e4
Ashley Bland <ashleyjbland@gmail.com>
2018-09-28 21:25:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
   extend Memorable::ClassMethods
-  include Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable::ClassMethods
   include Paramable
 
